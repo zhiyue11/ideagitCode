7b65af98cc600260092de8e04cdf654baf69b650
zhi <1608434326@qq.com>
2022-06-05 00:58:53 +0800

    TestIt second commit

--- src/test/java/TestIt.java
+++ src/test/java/TestIt.java
@@ -3,6 +3,9 @@ import org.junit.Test;
 public class TestIt {
     @Test
     public void test01(){
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
 
     }
 
